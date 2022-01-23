92d91c2ac3b153a814ee39acdafd58847eb3d9bd
GhastyCZ <franc.matyas@gmail.com>
2022-01-23 14:40:19 +0100

    loginwin2 je pmomentálně primární

---
+++ login.html.r
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODO | Log in</title>
+    <link rel="stylesheet" href="login-style.css">
+</head>
+<body>
+    <div class="main-div">
+        <div class="login-div">
+            <h1>log in</h1>
+            <div class="line"></div>
+            <form action="" class="login-form">
+                <img src="Icons/account-white.png" alt="username icon" height="15px">
+                <label for="Username"><strong>Username</strong></label> <br>
+                <input type="text" name="username" class="input-username"> <br>
+                <img src="Icons/padlock-white.png" alt="password icon" height="15px">
+                <label for="Password"><strong>Password</strong></label> <br>
+                <input type="password" name="password" class="input-password"> <br>
+                <input type="submit" value="Submit" class="input-submit">
+            </form>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <img src="Icons/account-white.png" alt="username icon attribution" height="10px">
+        <img src="Icons/padlock-white.png" alt="password icon attribution" height="10px">
+        <p><a href="https://www.flaticon.com/free-icons/user" title="user icons" >User icons created by Phoenix Group - Flaticon</a></p>
+        <p class="pipeline">|</p>
+    </footer>
+</body>
+</html>
