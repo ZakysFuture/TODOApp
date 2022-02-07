98c96000d8366aa1c72051447477a9de968e9582
Matyas Franc <franc.matyas@gmail.com>
2022-02-07 23:25:41 +0100

    taskCount filler

--- js/todo-script.js
+++ js/todo-script.js
@@ -1,3 +1,25 @@
+$(document).ready(function() {
+    let taskCount = [100,5,0,3,13,0,2] 
+    let taskCountDiv = ["#task-count-0","#task-count-1","#task-count-2","#task-count-3","#task-count-4","#task-count-5","#task-count-6"]
+
+    for (var i in taskCount)
+    {
+        if (taskCount[i] === 0)
+        {
+            $(taskCountDiv[i]).hide()
+        }
+        else if (taskCount[i] > 99)
+        {
+            $(taskCountDiv[i]).text("99")
+        }
+        else
+        {
+            $(taskCountDiv[i]).text(taskCount[i])
+        }
+    }
+})
+
+
 $(function(){
     $(".header-menu").click(function(){
         if ($(".menu-div").is(":visible"))
