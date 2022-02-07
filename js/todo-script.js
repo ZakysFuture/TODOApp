e2069da65c4f6ae2aafff0c43c9ff13e76c92e74
Matyas Franc <franc.matyas@gmail.com>
2022-02-07 22:38:26 +0100

    working sidebar hide

--- js/todo-script.js
+++ js/todo-script.js
@@ -1,19 +1,28 @@
 $(function(){
-    
+    $(".header-menu").click(function(){
+        if ($(".menu-div").is(":visible"))
+        {
+            $(".menu-div").hide();
+        }
+        else
+        {
+            $(".menu-div").show();
+        }
+    })
 })
 
 $(function(){
     $(".task-sidebar-button").click(function(){
         if ($(".task-sidebar-content").is(":visible"))
-            {
-                $(".task-sidebar-content").hide()
-                $(".task-div-center").css("width", "100%")
-            }
-            else
-            {
-                let taskDivCenterWidth = ($(".task-div").css("width")) - $(".task-sidebar").css("width");
-                $(".task-sidebar-content").show()
-                $(".task-div-center").css("width", "80%")
-            }
+        {
+            $(".task-sidebar-content").hide()
+            $(".task-div-center").css("width", "100%")
+        }
+        else
+        {
+            let taskDivCenterWidth = ($(".task-div").css("width")) - $(".task-sidebar").css("width");
+            $(".task-sidebar-content").show()
+            $(".task-div-center").css("width", "100%")
+        }
     });
 })
