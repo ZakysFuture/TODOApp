5b3d53aa47f35b07537e1ed05fdf123e91f7a29e
Matyas Franc <franc.matyas@gmail.com>
2022-02-07 19:08:06 +0100

    sidebar hide and show (bad width)

--- js/todo-script.js
+++ js/todo-script.js
@@ -1,3 +1,19 @@
 $(function(){
     
 })
+
+$(function(){
+    $(".task-sidebar-button").click(function(){
+        if ($(".task-sidebar-content").is(":visible"))
+            {
+                $(".task-sidebar-content").hide()
+                $(".task-div-center").css("width", "100%")
+            }
+            else
+            {
+                let taskDivCenterWidth = ($(".task-div").css("width")) - $(".task-sidebar").css("width");
+                $(".task-sidebar-content").show()
+                $(".task-div-center").css("width", "80%")
+            }
+    });
+})
